a8b0c86666f10a46f3952659ae5579017969fab0
Fadejimi <adegbulugbefadejimi@gmail.com>
2025-01-05 17:34:33 -0600

    Adding input box in player component

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,11 +1,15 @@
-export default function Player({name, symbol}) {
+import { useState } from "react";
+
+export default function Player({ name, symbol }) {
+    const [editing, setEditing] = useState(false);
+
     return (
         <li>
             <span className="player">
-                <span className="player-name">{name}</span>
+                {editing ? <input type="text" value={name} /> : <span className="player-name">{name}</span>}
                 <span className="player-symbol">{symbol}</span>
             </span>
-            <button>Edit</button>
+            <button onClick={() => setEditing(!editing)}>Edit</button>
         </li>
     );
 }
