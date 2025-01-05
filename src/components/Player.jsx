28f7fa12fc53a82039ebf29a3d25225c15c80176
Fadejimi <adegbulugbefadejimi@gmail.com>
2025-01-05 17:46:22 -0600

    Set the edit button name dynamically

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -2,14 +2,24 @@ import { useState } from "react";
 
 export default function Player({ name, symbol }) {
     const [editing, setEditing] = useState(false);
+    const [newName, setNewName] = useState(name);
+
+    function handleButtonClick() {
+        if (editing) {
+            setEditing(false);
+            setNewName(document.querySelector("input").value);
+        } else {
+            setEditing(true);
+        }
+    }
 
     return (
         <li>
             <span className="player">
-                {editing ? <input type="text" value={name} /> : <span className="player-name">{name}</span>}
+                {editing ? <input type="text" required /> : <span className="player-name">{newName}</span>}
                 <span className="player-symbol">{symbol}</span>
             </span>
-            <button onClick={() => setEditing(!editing)}>Edit</button>
+            <button onClick={handleButtonClick}>{editing ? "Save" : "Edit"}</button>
         </li>
     );
 }
