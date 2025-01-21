9e9960e69ba27ad9967531d7f01baa465f3ee1f7
Fadejimi <adegbulugbefadejimi@gmail.com>
2025-01-20 19:50:50 -0600

    Fix the logging state

--- src/components/Log.jsx
+++ src/components/Log.jsx
@@ -1,7 +1,13 @@
-export default function Log() {
+export default function Log({ turns }) {
     return (
         <div id="log">
-            <ol></ol>
+            <ol>
+                {turns.map(({ square, player }, index) => (
+                    <li key={index}>
+                        <span>{player} played at row {square.row}, column {square.col}</span>
+                    </li>
+                ))}
+            </ol>
         </div>
     );
 }
