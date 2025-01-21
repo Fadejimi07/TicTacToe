f25db2055e676e6ed3a1fe18a8365d1b8b0200f1
Fadejimi <adegbulugbefadejimi@gmail.com>
2025-01-20 18:47:12 -0600

    Creating the log component

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 
 import Player from "./components/Player";
 import GameBoard from "./components/Gameboard";
+import Log from "./components/Log";
 
 function App() {
   const [currentPlayer, setCurrentPlayer] = useState("X");
@@ -19,7 +20,7 @@ function App() {
         </ol>
         <GameBoard onSelectSquare={handlePlayerChange} currentPlayerSymbol={currentPlayer} />
       </div>
-      LOG
+      <Log />
     </main>
   );
 }
